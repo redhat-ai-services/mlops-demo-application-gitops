2b2776f06038f0743791cc09f2b0c9d42d54daaf
Trevor Royer <troyer@redhat.com>
2022-09-19 17:30:12 -0500

    init files

---
+++ bootstrap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+BOOTSTRAP_DIR="bootstrap/overlays/default/"
+ARGO_NS=mlops-demo-gitops
+
+# check login
+check_oc_login(){
+  oc cluster-info | head -n1
+  oc whoami || exit 1
+  echo
+
+  sleep 5
+}
+
+main(){
+    echo "Applying overlay: ${BOOTSTRAP_DIR}"
+    kustomize build ${BOOTSTRAP_DIR} | oc apply -f -
+
+    echo ""
+    echo "Deploying tenant instance.  Check the status of the sync here:
+    "
+    route=$(oc get route openshift-gitops-server -o=jsonpath='{.spec.host}' -n ${ARGO_NS})
+
+    echo "https://${route}"
+}
+
+check_oc_login
+
+main
